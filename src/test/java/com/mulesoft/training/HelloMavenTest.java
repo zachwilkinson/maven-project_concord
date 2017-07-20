2b0f822939f517e743a9c687bef600ce401cbfd2
NETWORK\zach.wilkinson <zach.wilkinson@concordusa.com>
2017-07-20 09:32:46 -0500

    added test retrieveFlightsAddsAppropriateHeader

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -18,6 +19,13 @@ public class HelloMavenTest extends FunctionalTestCase {
 		System.out.println("\n\n Dynamic Port in Test Case 1: " + dynamicPort.getNumber() + "\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
+	
+	@Test
+	 public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+	   MuleEvent event = runFlow("retrieveFlights");
+	   String contentType = event.getMessage().getOutboundProperty("Content-Type");
+	   assertEquals("application/json", contentType);
+	 }
     
     @Override
     protected String[] getConfigFiles() {
